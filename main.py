5cb360db42872035b561cc1c4fa5a07882c7dcd6
ForAryam <Mohammede7777@gmail.com>
2025-01-03 11:45:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def download_with_fallback(url):
             'quiet': True,
             'no_warnings': True,
             'format': 'best',
+            'cookiefile': 'cookies.txt',  # مسار ملف الكوكيز
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
